eff5719bb428d058bcf3c112e1cae08ec94bdb0a
cwook <cwookdev@gmail.com>
2017-10-19 14:01:13 -0700

    added adjustPartQuantity and setPartQuantity methods to DBHandler.java changed the way update and insert work so they are more robust removed extra bracket from StockroomApp.java

--- src/DBHandlerTest.java
+++ src/DBHandlerTest.java
@@ -7,18 +7,18 @@ public class DBHandlerTest {
 
     public static void main(String[] args){
         DBHandler testDB = new DBHandler();
-//        ResultSet result = testDB.select("stockroomdb.PARTS", "part_number", new ArrayList<String>());
-//        try{
-//            result.beforeFirst();
-//            int i = 0;
-//            while(result.next()){
-//                System.out.println("Row " + i + " part_id: " + result.getInt(1));
-//                i++;
-//            }
-//        }
-//        catch(SQLException e){z
-//            e.printStackTrace();
-//        }
+        ResultSet result = testDB.select("stockroomdb.PARTS", "part_number");
+        try{
+            result.beforeFirst();
+            int i = 0;
+            while(result.next()){
+                System.out.println("Row " + i + " part_id: " + result.getInt(1));
+                i++;
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
 //
 //        ArrayList<String> testArray = new ArrayList<String>();
 //        testArray.add("id INT ");
