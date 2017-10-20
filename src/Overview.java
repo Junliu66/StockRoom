b4da7e28209cf98938837dcfdc0febfdcdce250d
Stefano Mauri <stefanom@gmail.com>
2017-10-19 23:09:38 -0700

    Added SQL Queries for Overview commented

--- src/Overview.java
+++ src/Overview.java
@@ -28,6 +28,42 @@ public class Overview {
         ResultSet order_id = testDB.select("stockroomdb.WORKORDERS", "order_id", new ArrayList<String>());
         ResultSet status = testDB.select("stockroomdb.WORKORDERS", "status", new ArrayList<String>());
         ResultSet date_completed = testDB.select("stockroomdb.WORKORDERS", "date_completed", new ArrayList<String>());
+
+        // ###################################################################################
+        /* Stefano : These are queries I made up for the overview tables */
+        // ###################################################################################
+        /*
+
+        // ###################################################################################
+        // TABLE ONE - (This one shows all the Work Orders that are completed and ready to ship.)
+        // ###################################################################################
+
+        ResultSet table1_order_id = testDB.query("SELECT order_id FROM stockroomdb.WORKORDERS WHERE status = "COMPLETED";");
+        ResultSet table1_product_name = testDB.query("SELECT p.product_name FROM stockroomdb.PRODUCTS AS p JOIN stockroomdb.WORKORDERS AS wo ON p.product_id = wo.product_id WHERE status = "COMPLETED";");
+        ResultSet table1_date_completed = testDB.query("SELECT date_completed FROm stockroomdb.WORKORDERS WHERE status = "COMPLETED";");
+
+
+        // ###################################################################################
+        // TABLE TWO - (This one shows all the Work Orders that are currently being built.)
+        // ###################################################################################
+
+        ResultSet table2_order_id = testDB.query("SELECT order_id FROM stockroomdb.WORKORDERS WHERE status = "BUILDING";");
+        ResultSet table2_product_name = testDB.query("SELECT p.product_name FROM stockroomdb.PRODUCTS AS p JOIN stockroomdb.WORKORDERS AS wo ON p.product_id = wo.product_id WHERE status = "BUILDING";");
+        ResultSet table2_date_building = testDB.query("SELECT date_building FROM stockroomdb.WORKORDERS WHERE status = "BUILDING";");
+
+
+        // ###################################################################################
+        // TABLE THREE - (This one is pretty much the same as the Purchasing table, it says what parts are missing from WorkOrders.)
+        // ###################################################################################
+
+        ResultSet table3_parts_id = testDB.query("SELECT DISTINCT p.parts_id FROM stockroomdb.PARTS AS p JOIN stockroomdb.STOCKROOM AS s ON p.parts_id = s.parts_id JOIN stockroomdb.ORDER_ITEMS AS oi ON s.parts_id = oi.parts_id WHERE s.quantity < (oi.amount_needed - oi.amount_filled);");
+        ResultSet table3_part_description = testDB.query("SELECT DISTINCT p.part_description FROM stockroomdb.PARTS AS p JOIN stockroomdb.STOCKROOM AS s ON p.parts_id = s.parts_id JOIN stockroomdb.ORDER_ITEMS AS oi ON s.parts_id = oi.parts_id WHERE s.quantity < (oi.amount_needed - oi.amount_filled);");
+        ResultSet table3_part_vendor = testDB.query("SELECT DISTINCT p.vendor FROM stockroomdb.PARTS AS p JOIN stockroomdb.STOCKROOM AS s ON p.parts_id = s.parts_id JOIN stockroomdb.ORDER_ITEMS AS oi ON s.parts_id = oi.parts_id WHERE s.quantity < (oi.amount_needed - oi.amount_filled);");
+        ResultSet table3_order_missing_quantity = testDB.query("SELECT s.quantity - SUM(oi.amount_needed - oi.amount_filled) FROM stockroomdb.PARTS AS p JOIN stockroomdb.STOCKROOM AS s ON p.parts_id = s.parts_id JOIN stockroomdb.ORDER_ITEMS AS oi ON s.parts_id = oi.parts_id WHERE s.quantity < (oi.amount_needed - oi.amount_filled);");
+
+        */
+
+
         try {
             order_id.beforeFirst();
             status.beforeFirst();
