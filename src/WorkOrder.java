c3f25f3a507786e32439f7a2c150d5e8011a298c
Stefano Mauri <stefanom@gmail.com>
2017-10-26 16:44:49 -0700

    Added Kitting to WorkOrders

--- src/WorkOrder.java
+++ src/WorkOrder.java
@@ -119,7 +119,121 @@ public class WorkOrder {
     }
 
     private void kitWorkOrder() {
-        System.out.println(" kit ");
+        ResultSet orderID = stockroomDB.query("SELECT order_id FROM stockroomdb.WORKORDERS WHERE status = 'CREATED';");
+        ResultSet productName = stockroomDB.query("SELECT p.product_name FROM stockroomdb.WORKORDERS as wo JOIN stockroomdb.PRODUCTS as p ON wo.product_id = p.product_id WHERE status = 'CREATED';");
+        ResultSet quantity = stockroomDB.query("SELECT quantity FROM stockroomdb.WORKORDERS WHERE status = 'CREATED';");
+
+        try {
+            orderID.beforeFirst();
+            productName.beforeFirst();
+            quantity.beforeFirst();
+
+            System.out.println("LIST OF KITS STILL IN NEED OF KITTING:\n");
+            System.out.println("=================================================================");
+            System.out.printf("||%-8s |%-40s |%-9s||", "ORDER ID", "              PRODUCT NAME", " QUANTITY");
+            System.out.println("\n=================================================================");
+
+            while(orderID.next()){
+                productName.next();
+                quantity.next();
+
+                System.out.printf("|%9d |%-40s |%10d|\n", orderID.getInt(1), productName.getString(1), quantity.getInt(1));
+            }
+            System.out.println("-----------------------------------------------------------------\n");
+
+            System.out.println("Select the order ID that you want to kit: ");
+            int chosenOrderID = reader.nextInt();
+
+            orderID.beforeFirst();
+            boolean orderFound = false;
+
+            while(orderID.next()){
+                if (chosenOrderID == orderID.getInt(1)){
+                    orderFound = true;
+                    System.out.println("Order found. Type QUIT to stop kitting. ");
+                    kitting(chosenOrderID);
+                }
+            }
+            if (!orderFound)
+                System.out.println("Order ID not found.");
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        ResultSet amount_needed = stockroomDB.select( "stockroomdb.ORDER_ITEMS", "amount_needed", new ArrayList<>());
+        ResultSet amount_filled = stockroomDB.select( "stockroomdb.ORDER_ITEMS", "amount_filled", new ArrayList<>());
+
+
+    }
+
+    private void kitting(int chosenOrderID) {
+        ResultSet parts_ID = stockroomDB.query("SELECT parts_id FROM stockroomdb.ORDER_ITEMS WHERE order_id = " + chosenOrderID + " AND amount_needed > amount_filled;");
+        ResultSet part_description = stockroomDB.query("SELECT p.part_description FROM stockroomdb.ORDER_ITEMS as oi JOIN stockroomdb.PARTS as p ON oi.parts_id = p.parts_id WHERE order_id = " + chosenOrderID + " AND amount_needed > amount_filled;");
+        ResultSet amount_needed = stockroomDB.query("SELECT amount_needed FROM stockroomdb.ORDER_ITEMS WHERE order_id = " + chosenOrderID + " AND amount_needed > amount_filled;");
+        ResultSet amount_filled = stockroomDB.query("SELECT amount_filled FROM stockroomdb.ORDER_ITEMS WHERE order_id = " + chosenOrderID + " AND amount_needed > amount_filled;");
+        try {
+            parts_ID.beforeFirst();
+            part_description.beforeFirst();
+            amount_needed.beforeFirst();
+            amount_filled.beforeFirst();
+
+            System.out.println("LIST OF KITS STILL IN NEED OF KITTING:\n");
+
+
+            while (parts_ID.next()) {
+                part_description.next();
+                amount_needed.next();
+                amount_filled.next();
+
+                System.out.println("==================================================================================================");
+                System.out.printf("||%-8s |%-50s |%-15s |%-15s||", "PARTS ID", "          PART DESCRIPTION", " AMOUNT NEEDED", " AMOUNT FILLED");
+                System.out.println("\n==================================================================================================");
+                System.out.printf("|%9d |%-50s |%15d |%16d|\n", parts_ID.getInt(1), part_description.getString(1), amount_needed.getInt(1), amount_filled.getInt(1));
+                System.out.println("--------------------------------------------------------------------------------------------------\n");
+                int kitfill = fillKit(chosenOrderID, parts_ID.getInt(1));
+            }
+            if (!parts_ID.next()) {
+                System.out.println("KITTING COMPLETE!");
+                int workOrderStatusChange = stockroomDB.updateQuery("UPDATE stockroomdb.WORKORDERS SET status = 'KITTED', date_kitted = NOW() WHERE order_id = " + chosenOrderID + ";");
+            }
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private int fillKit(int chosenOrderID, int parts_ID) {
+        System.out.println("FILL COMPLETELY? Y OR N");
+        String choice = reader.next();
+        boolean finished = false;
+
+        System.out.println(choice);
+        while (!finished)
+        {
+            if (choice.equals("Y")) {
+                System.out.println("adding to kit...");
+                int fill_part = stockroomDB.updateQuery("UPDATE stockroomdb.ORDER_ITEMS SET amount_filled = amount_needed WHERE order_id = " + chosenOrderID + " AND parts_id = " + parts_ID + ";");
+                return 1;
+            } else if (choice.equals("N")) {
+                System.out.println("PARTIAL FILL? Y OR N");
+                String secondChoice = reader.next();
+                if (secondChoice.equals("Y")) {
+                    System.out.println("HOW MANY TO FILL? : ");
+                    int amountToFill = reader.nextInt();
+                    int partial_fill = stockroomDB.updateQuery("UPDATE stockroomdb.ORDER_ITEMS SET amount_filled = " + amountToFill + " WHERE order_id = " + chosenOrderID + " AND parts_id = " + parts_ID + ";");
+                    return 2;
+                }
+            } else if (choice.equals("QUIT")) {
+
+                return 0;
+            } else {
+                System.out.println("Could not read choice. Choose just Y or N or type QUIT to leave");
+            }
+        }
+        return 0;
     }
 
     private void buildWorkOrder() {
